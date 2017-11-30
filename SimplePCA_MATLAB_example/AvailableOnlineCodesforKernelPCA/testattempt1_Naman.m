cd72f8d15fd3cdc1c84bbc03774f711161014a53
Shukla <namans2@illinois.edu>
2017-11-29 18:21:02 -0600

    added USPS display file

--- SimplePCA_MATLAB_example/AvailableOnlineCodesforKernelPCA/testattempt1_Naman.m
+++ SimplePCA_MATLAB_example/AvailableOnlineCodesforKernelPCA/testattempt1_Naman.m
@@ -1,12 +1,12 @@
 load('usps_all')
 X = data;
 
-% for i=1:10
-%     X_test = X(:,:,i);
-%     X_test = X_test';
-%     displayData(X_test(1:1100,:));
-%     pause;
-% end
+for i=1:10
+    X_test = X(:,:,i);
+    X_test = X_test';
+    displayData(X_test(1:1100,:));
+    pause;
+end
 X_test = X(:,:,1);
 X_test = X_test';
 %[mappedX, mapping, k, L, V] = kernel_pca(double(X_test(1:500,:)), 256);
